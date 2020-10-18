b2987ab2122da2f30a3760cd7cad793fd11683d8
Francin Anoj Vincent <francinv@stud.ntnu.no>
2020-10-19 01:55:43 +0200

    deleted header and footer

---
+++ modularity/footer.js
@@ -0,0 +1,10 @@
+var footer=document.querySelector("footer");
+var head=document.querySelector("head");
+
+function modfooter(){
+    head.innerHTML+=`<link rel="stylesheet" type="text/css" href="css/style.css">`;
+
+    footer.innerHTML = `
+    `
+
+}
