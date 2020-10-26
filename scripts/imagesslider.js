14695f859def55b3dcaa47aa7bd90e3cd42319c5
Francin Anoj Vincent <francinv@stud.ntnu.no>
2020-10-26 20:04:12 +0100

    Update

--- scripts/imagesslider.js
+++ scripts/imagesslider.js
@@ -1,31 +1,24 @@
-<script>
-  var i = 0;
-  var image = [];
-  var time = 40000;
+var i = 0;
+var image = [];
+var time = 40000;
 
-  images[0] = 'img1.jpg';
-  image[1] =
-  image[2] =
-  image[3] =
-  image[4] =
-  image[5] =
-  image[6] =
+images[0] = '../img/imagesslider/img1.jpg';
+image[1] =  '../img/imagesslider/img2.jpg'
+image[2] = '../img/imagesslider/img3.jpg'
+  
 
-  function vhangeImg(){
-    document.img.src = image[i];
+function changeImg(){
+  document.img.src = image[i];
 
-    if(i < image.lengt - 1){
-      i++;
-    } else{
-      i = 0;
-    }
-
-    setTimeout("changeImg()"), time)
+  if(i < image.lengt - 1){
+    i++;
+  } else{
+    i = 0;
   }
 
-  window.onload = changeImg;
+  setTimeout("changeImg()"), time)
+}
+window.onload = changeImg();
 
 
-</script>
 
-<img name="img" width="100%" height="100%">
