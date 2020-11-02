185eaac65069a1a50105c4f211267c0f26d05971
Francin Anoj Vincent <francinv@stud.ntnu.no>
2020-11-02 15:53:48 +0100

    done with utleier.html and extendfield.js

--- scripts/extendfield.js
+++ scripts/extendfield.js
@@ -1,18 +1,93 @@
-function extendField() {
-    var show1 = document.getElementById("befaring_field_text");
-    var show2 = document.getElementById("utleie_field_text")
-    if (show1.style.display === "none") {
-        show1.style.display = "block";
+var click1 = document.getElementById("befaring_field_header");
+click1.addEventListener("click", extendField1);
+var click2 = document.getElementById("utleie_field_header");
+click2.addEventListener("click", extendField2);
+var click3 = document.getElementById("innflytting_field_header");
+click3.addEventListener("click", extendField3);
+var click4 = document.getElementById("oppfolging_field_header");
+click4.addEventListener("click", extendField4);
+var click5 = document.getElementById("juridisk_field_header");
+click5.addEventListener("click", extendField5);
+var click6 = document.getElementById("mypage_field_header");
+click6.addEventListener("click", extendField6);
+var click7 = document.getElementById("economy_field_header");
+click7.addEventListener("click", extendField7);
+var click8 = document.getElementById("befaringafter_field_header");
+click8.addEventListener("click", extendField8);
+var click9 = document.getElementById("utflytting_field_header");
+click9.addEventListener("click",extendField9);
+
+function extendField1(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[0].style.display === "none"){
+        (extend[0].style.display = "block");
     } else {
-        show1.style.display = "none";
+        extend[0].style.display = "none";
     }
-    if (show2.style.display === "none"){
-        show2.style.display ="block";
-    }else{
-        show2.style.display="block";
+}
+function extendField2(){
+    var extend = document.getElementsByClassName("field_text");
+
+    if (extend[1].style.display === "none"){
+        extend[1].style.display ="block";
+    } else {
+        extend[1].style.display ="none";
     }
-};
+}
+function extendField3(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[2].style.display === "none"){
+        extend[2].style.display ="block";
+    } else {
+        extend[2].style.display ="none";
+    }
+}
+function extendField4(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[3].style.display === "none"){
+        extend[3].style.display ="block";
+    } else {
+        extend[3].style.display ="none";
+    }
+}
+function extendField5(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[4].style.display === "none"){
+        extend[4].style.display ="block";
+    } else {
+        extend[4].style.display ="none";
+    }
+}
+function extendField6(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[5].style.display === "none"){
+        extend[5].style.display ="block";
 
-
-
-
+    } else {
+        extend[5].style.display ="none";
+    }
+}
+function extendField7(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[6].style.display === "none"){
+        extend[6].style.display ="block";
+    } else {
+        extend[6].style.display ="none";
+    }
+}
+function extendField8(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[7].style.display === "none"){
+        extend[7].style.display ="block";
+    } else {
+        extend[7].style.display ="none";
+    }
+}
+function extendField9(){
+    var extend = document.getElementsByClassName("field_text");
+    if (extend[8].style.display === "none"){
+        extend[8].style.display ="block";
+    } else {
+        extend[8].style.display ="none";
+    }
+}
